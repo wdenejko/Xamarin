6665be7944bc5ce59ad90942e0eef9a1ef72db69
Wojtek Denejko <deneyw@gmail.com>
2016-11-24 00:22:58 +0100

    Android deploy fix

--- OcrApp/Droid/MainActivity.cs
+++ OcrApp/Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using XLabs.Platform.Device;
 
 namespace OcrApp.Droid
 {
-	[Activity(Label = "OcrApp.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Label = "OcrApp.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate(Bundle bundle)
